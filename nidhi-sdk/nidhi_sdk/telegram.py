efe80f019f73585d18a4980cd801aae1639f9766
aadisheshu <safacts001@gmail.com>
2026-07-03 17:12:23 +0530

    SCRUM-295: Update Telegram SDK to support TELEGRAM_TOKEN

--- nidhi-sdk/nidhi_sdk/telegram.py
+++ nidhi-sdk/nidhi_sdk/telegram.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def send_telegram_alert(message: str) -> None:
-    token = os.environ.get('TELEGRAM_BOT_TOKEN')
+    token = os.environ.get('TELEGRAM_BOT_TOKEN') or os.environ.get('TELEGRAM_TOKEN')
     chat_id = os.environ.get('TELEGRAM_CHAT_ID')
     
     if not token or not chat_id:
